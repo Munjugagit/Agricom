c7f530f33254eded0e646a95dff1a0d1f8898fd0
Munjugagit <alicemunjuga@gmail.com>
2022-03-01 15:35:22 +0300

    first commit

---
+++ agricom/reporter/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Incidences
+# Register your models here.
+
+class IncidencesAdmin(admin.ModelAdmin):
+    # pass
+   list_display =('name','location')
+
+
+admin.site.register(Incidences, IncidencesAdmin)    
---
+++ agricom/reporter/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.gis.db import models
+
+
+# Create your models here.
+class Incidences(models.Model):
+    name = models.CharField(max_length=20)
+    location = models.PointField(srid=4326)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural ="Incidences"
