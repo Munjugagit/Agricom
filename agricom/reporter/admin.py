48c2345858dce62bbf1582a44ed1d098b4194cb8
Munjugagit <alicemunjuga@gmail.com>
2022-03-02 22:51:19 +0300

    Second Commit

--- agricom/reporter/admin.py
+++ agricom/reporter/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import Incidences
+from django.contrib.gis import admin
+from leaflet.admin import LeafletGeoAdmin
+
 # Register your models here.
 
-class IncidencesAdmin(admin.ModelAdmin):
+class IncidencesAdmin(LeafletGeoAdmin):
     # pass
    list_display =('name','location')
 
